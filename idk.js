0432cd438a7f0e59fe757b2199362dbebf8e7c04
Avery Pham <phamavery@gmail.com>
2020-08-17 16:27:29 -0700

    trying to add api stuff

--- idk.js
+++ idk.js
@@ -19,8 +19,23 @@ document.getElementById("selectchamber").addEventListener("change", function (e)
 		document.getElementById("senator").className = "initially-invisible";
 	}
 });
-
+const congressNumber = 116;
+var whatToFetch = 'https://api.propublica.org/congress/v1/' + congressNumber + '/' + myChamber + '/members.json';
 function search() {
-	console.log('Hello World');
-}
+	fetch(whatToFetch, {
+	headers: {
+		'X-API-Key': 'API-KEY',
+	},
+	})
+	.then(response => response.json())
+	.then(data => {
+	var names = [];
+	for(const {first_name, last_name} of data.results.members){
+		names.append({first_name,last_name});
+	}
+	})
+	.catch((error) => {
+	console.error('Error:', error);
+	});
 
+}
